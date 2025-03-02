efc5ed895c55b9389644eb20c1d447ab07b94b2b
Steven Lim <limstevenlbw@gmail.com>
2025-03-02 05:03:44 -0800

    fixed bug with navbar flex layout

--- src/views/navbar/Navbar.tsx
+++ src/views/navbar/Navbar.tsx
@@ -6,95 +6,97 @@ import './Navbar.css'
 const Navbar = ({ user: any }) => {
     return (
         <nav className="navbar-bg navbar navbar-expand-lg navbar-light">
+            <div className="container-fluid">
+                <div className="navbar-header">
+                    <NavLink
+                        className="navbar-brand"
+                        to="/">
 
-            <Link
-                className="navbar-brand pl-4"
-                to="/">
-                DeckRefactory
-            </Link>
+                        <img className="navbarIcon" src={navbarIcon} alt="">
+                        </img>
 
-            <img className="navbarIcon" src={navbarIcon} alt="">
-            </img>
+                        <span>DeckRefactory Alpha</span>
+                    </NavLink>
+                </div>
 
-            <div>&nbsp; alpha preview</div>
+                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#collapsibleNavbar">
+                    <span className="navbar-toggler-icon"></span>
+                </button>
 
-            <button
-                className="navbar-toggler"
-                type="button"
-                data-toggle="collapse"
-                data-target="#navbarSupportedContent"
-                aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                <span className="navbar-toggler-icon"></span>
-            </button>
+                <div className="collapse navbar-collapse " id="collapsibleNavbar">
+                    <div className="navbar-custom-mr">
+                        <ul className="nav navbar-nav navbar-custom-mr2">
+                            <li className="nav-item">
+                                <NavLink
+                                    className="nav-link"
+                                    to="/">
+                                    Home
+                                </NavLink>
+                            </li>
 
-            <div className="collapse navbar-collapse pr-4" id="navbarSupportedContent">
-                <ul className="navbar-nav ml-auto"> {/*Set ml-auto to right align*/}
-                    <li className="nav-item">
-                        <NavLink
-                            className="nav-link"
-                            to="/">
-                            Home
-                        </NavLink>
-                    </li>
+                            <li className="nav-item">
+                                <NavLink
+                                    className="nav-link"
+                                    to="/builder">
+                                    Build a Deck
+                                </NavLink>
+                            </li>
 
-                    <li className="nav-item">
-                        <NavLink
-                            className="nav-link"
-                            to="/builder">
-                            Build a Deck
-                            <span className="sr-only">(current)</span>
-                        </NavLink>
-                    </li>
+                            <li className="nav-item">
+                                <NavLink
+                                    className="nav-link"
+                                    to="/manager">
+                                    Manage Decks
+                                </NavLink>
+                            </li>
 
-                    <li className="nav-item">
-                        <NavLink
-                            className="nav-link"
-                            to="/manager">
-                            Manage Decks
-                        </NavLink>
-                    </li>
+                            <li className="nav-item">
+                                <NavLink className="nav-link" to="/copyright">
+                                    Copyright
+                                </NavLink>
+                            </li>
+                        </ul>
 
-                    <li className="nav-item">
-                        <NavLink className="nav-link" to="/copyright">
-                            Copyright
-                        </NavLink>
-                    </li>
-                </ul>
+                        <ul className="nav navbar-nav navbar-custom-mr3 ">
+                            {false ?
+                                (<React.Fragment>
+                                    <li className="nav-item">
+                                        <NavLink className="nav-link" to="/profile">
+                                            Profile
+                                        </NavLink>
+                                    </li>
+
+                                    <li className="nav-item">
+                                        <NavLink className="nav-link" to="/logout">
+                                            Sign Out
+                                        </NavLink>
+                                    </li>
+                                </React.Fragment>)
+                                :
+                                (<React.Fragment>
+                                    <li className="nav-item">
+                                        <NavLink className="nav-link" to="/register">
+                                            Register
+                                        </NavLink>
+                                    </li>
+
+                                    <li className="nav-item">
+                                        <NavLink className="nav-link" to="/login">
+                                            Login
+                                        </NavLink>
+                                    </li>
+                                </React.Fragment>)
+                            }
+
+                        </ul>
+                    </div>
+
+                </div>
             </div>
+
         </nav>
     );
 }
 
-/*
-                    {user ?
-                        (<React.Fragment>
-                            <li className="nav-item">
-                                <NavLink className="nav-link" to="/profile">
-                                    Profile
-                                </NavLink>
-                            </li>
 
-                            <li className="nav-item">
-                                <NavLink className="nav-link" to="/logout">
-                                    Sign Out
-                                </NavLink>
-                            </li>
-                        </React.Fragment>)
-                        :
-                        (<React.Fragment>
-                            <li className="nav-item">
-                                <NavLink className="nav-link" to="/register">
-                                    Register
-                                </NavLink>
-                            </li>
-
-                            <li className="nav-item">
-                                <NavLink className="nav-link" to="/login">
-                                    Login
-                                </NavLink>
-                            </li>
-                        </React.Fragment>)
-                    }
-
-                    */
 export default Navbar;
